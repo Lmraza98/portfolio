08e814e519cb19d85bc1b77e30b40d13d2840ff2
lur057 <lucas_raza@harvard.edu>
2020-12-27 14:53:30 -0500

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,14 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 import React from 'react'
-import Footer from '../components/Footer/Footer.js'
 import Navbar from '../components/Navbar/Navbar.js'
 import MainContent from '../components/MainContent/MainContent.js'
-import { usePageState } from '../page-context.js'
 
-const MainContent_container = styled.div`
-  display: inline-block;
-  justify-content: center;
-  position: relative;
-  
-  height: 100%;
-`
 const Navbar_item = styled(Navbar)`
   display: block;
   height: 100%;
   position:fixed;
   
-`;
-const Footer_item = styled(Footer)`
-  grid-row: 3 / span 1;
-  grid-column:3;
-  z-index: 100;
-  position: fixed;
-  bottom: 0;
-  z-index: 20;
 `;
 const Background = styled(Image)`
   z-index: 10;
@@ -35,7 +18,7 @@ const Background = styled(Image)`
 `
 const Portfolio_container = styled.div
 `
-  position: absolute;
+  position: fixed;
   height: 100%;
   width: 100%;
   display: inline-block;
@@ -43,7 +26,6 @@ const Portfolio_container = styled.div
   transition: 2s;
 `;
 const Container = styled.div`
-// display: block;
   position: relative;
   width: 100%;
   height: 100%;
