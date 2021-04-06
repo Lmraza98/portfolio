85caa4e56f1fa1c83c2565af6f17c5d5d89a4968
Lucas Raza <Lmraza98@gmail.com>
2021-04-05 20:20:46 -0400

    added nginx.conf

--- chatbot/app.js
+++ chatbot/app.js
@@ -1,16 +1,51 @@
+const https = require('https')
 const express = require('express')
 const { WebhookClient } = require('dialogflow-fulfillment')
 const app = express()
+var fs = require('fs');
+var options = {
+  // Specify the key file for the server
+  key: fs.readFileSync('/etc/ssl/lucasraza.com.key'),
+  // Specify the certificate file
+  cert: fs.readFileSync('/etc/ssl/cloudflare.crt'),
+  // Specify the Certificate Authority certificate
+  ca: fs.readFileSync('./ca-crt.pem'),
+  // Requesting the client to provide a certificate, to authenticate the user.
+  requestCert: true,
+  // As specified as "true", so no unauthenticated traffic
+  // will make it to the specified route specified
+  rejectUnauthorized: false
+};
 app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("Server Is Working......")
 })
+
+app.use(function (req, res, next) {
+    if (!req.client.authorized) {
+      //return res.status(401).send('Client cert failed. User is not authorized\n');
+    }
+    // Examine the cert itself, and even validate based on that!
+    var cert = req.socket.getPeerCertificate();
+    if (cert.subject) {
+      console.log('Client Certificate: ',cert);
+      console.log('Client Certificate Common Name: '+cert.subject.CN);
+      console.log('Client Certificate Location: '+cert.subject.L);
+      console.log('Client Certificate Organization Name: '+cert.subject.O);
+      console.log('Client Certificate Email Address: '+cert.subject.emailAddress);
+    }
+  
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("hello world from client cert\n");
+    next();
+  });
 
 /**
 * on this route dialogflow send the webhook request
 * For the dialogflow we need POST Route.
 * */
-app.post('/webhook', (req, res) => {
+app.post('/dialogflow', (req, res) => {
     // get agent from request
     let agent = new WebhookClient({request: req, response: res})
 
@@ -31,6 +66,6 @@ function handleWebHookIntent(agent){
 /**
 * now listing the server on port number 3000 :)
 * */
-app.listen(4000, () => {
-    console.log("Server is Running on port 4000")
-})
+var listener = https.createServer(options, app).listen(4000, function () {
+  console.log('Express HTTPS server listening on port ' + listener.address().port);
+});
