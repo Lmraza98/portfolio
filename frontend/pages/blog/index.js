c4f5f6842945a4d0b40ae5eae31b0430329731ad
Lucas Raza <Lmraza98@gmail.com>
2021-04-30 01:40:24 -0400

    sucessful build

--- frontend/pages/blog/index.js
+++ frontend/pages/blog/index.js
@@ -1,13 +1,13 @@
 import dynamic from 'next/dynamic'
 import isMobile from '../../app/utilities/isMobile'
 
-const BlogDesktopView = dynamic (() => import('../../app/views/desktop').then( (mod) => (mod.BlogDesktopView ) ))
-const BlogMobileView = dynamic (() => import('../../app/views/mobile').then( (mod) => (mod.BlogMobileView ) ))
+// const BlogDesktopView = dynamic (() => import('../../app/views/desktop').then( (mod) => (mod.BlogDesktopView ) ))
+// const BlogMobileView = dynamic (() => import('../../app/views/mobile').then( (mod) => (mod.BlogMobileView ) ))
 
 function BlogPage (props) {
   const isMobileDevice = isMobile();
-
-  return (isMobileDevice ? <BlogMobileView/> : <BlogDesktopView/>)
+  return <div></div>
+  // return (isMobileDevice ? <BlogMobileView/> : <BlogDesktopView/>)
 }
 
 export default BlogPage;
