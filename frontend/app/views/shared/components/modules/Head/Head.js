bb7a598d2601bf5d264808d18a617d910c607df8
Lucas Raza <Lmraza98@gmail.com>
2021-04-30 01:42:01 -0400

    removed next-google-fonts

--- frontend/app/views/shared/components/modules/Head/Head.js
+++ frontend/app/views/shared/components/modules/Head/Head.js
@@ -1,11 +1,9 @@
 import NextHead from "next/head";
-import { GoogleFonts } from "next-google-fonts";
 import * as React from "react";
 
 export const Head = ({ title }) => {
     return (
         <React.Fragment>
-            <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
             <NextHead>
             <meta charSet="UTF-8" />
             <meta
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,10 +5,8 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
-import { GoogleFonts } from "next-google-fonts";
 export const Head = ({ children, title }) => (
   <React.Fragment>
-    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
     <NextHead>
 
       {children}
